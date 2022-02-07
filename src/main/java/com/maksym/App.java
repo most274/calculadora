78410efe5bec31d2c885c0b5b71d8f8428304d78
blas377 <bruelop377@g.educaand.es>
2022-02-07 14:22:39 +0100

    estructura menu

--- src/main/java/com/maksym/App.java
+++ src/main/java/com/maksym/App.java
@@ -1,5 +1,7 @@
 package com.maksym;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        Integer opcion = 0;
+
+        do {
+            System.out.print( "Introduzca una opción: " );
+            System.out.println("0 para salir.");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println( "Opción 1" );
+                    break;
+                default:
+                    break;
+            }
+
+        } while (opcion != 0);
+
+
+        
     }
 }
