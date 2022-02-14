32b97ad4b48410aaeb000d08d4d0a2a477f994cd
most274 <92079376+most274@users.noreply.github.com>
2022-02-14 13:42:36 +0100

    Pitagoras y Poligono Regular

---
+++ src/main/java/com/maksym/funciones/FuncionPoligonoRegular.java
@@ -0,0 +1,32 @@
+package com.maksym.funciones;
+
+import java.util.Scanner;
+
+public class FuncionPoligonoRegular {
+    static Scanner sc = new Scanner(System.in);
+
+    static public String calcular() {
+        String sol;
+        double num, lon, apo;
+
+        //Se introducen los datos
+        System.out.println("Introduzca el número de lados: ");
+        num = Double.parseDouble(sc.next());
+
+        System.out.println("Introduzca la longitud de los lados: ");
+        lon = Double.parseDouble(sc.next());
+
+        System.out.println("Introduzca el apótema: ");
+        apo = Double.parseDouble(sc.next());
+
+        //Compruba que sea mayor a 0 el número
+        if (num < 0 || lon < 0 || apo < 0) {
+            sol = "Datos incorrectos";
+        } else {
+            sol = String.valueOf((num * lon * apo) / 2);
+        }
+
+        return sol;
+    }
+
+}
