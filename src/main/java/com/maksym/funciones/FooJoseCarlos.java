ba6870ff268bcfb3cb380275729d51ff1f3d0fde
José Carlos <jvazfue651@g.educaand.es>
2022-03-07 14:01:48 +0100

    Factorial Jose Carlos

--- src/main/java/com/maksym/funciones/FooJoseCarlos.java
+++ src/main/java/com/maksym/funciones/FooJoseCarlos.java
@@ -2,4 +2,17 @@ package com.maksym.funciones;
 
 public class FooJoseCarlos {
     
+    static public void factorialbucle(int numero) {  
+        long sol=1;
+        
+        for(int i = numero; i >1; i--) {
+            sol = sol * i;
+        }
+        
+        System.out.println("Solución: " + sol);
+    }
+
+
+
+
 }
